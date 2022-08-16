a0581d63f1a9a19354f2ea2b4f07f42578f448ff
Jair-Delgado <jar.delgado@yavirac.edu.ec>
2022-08-16 10:44:07 -0500

    modificaciones al home

--- vistas/home.php
+++ vistas/home.php
@@ -1,7 +1,28 @@
-<div class="container is-fluid">
+<!--<div class="container is-fluid">
 	<h1 class="title">Hola</h1>
 	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
-</div>
+</div>-->
 
+<div class="card">
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img src="./img/perfil.webp" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+	  <h1 class="title">Hola</h1>
+	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+      </div>
+    </div>
+  </div>
+</div> <br>
 
+<hr>
 
+<?php
+
+include "departures_list.php";
+
+?>
